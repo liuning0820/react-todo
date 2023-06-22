ee34b13d78a78ec993e231ae8692f4cd5060c3f5
liuning0820 <liuning0820@outlook.com>
2023-06-22 08:58:50 +0800

    update todo app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,30 +1,73 @@
-import {useState} from 'react';
-import Modal from './Modal';
-import Backdrop from './Backdrop';
+import React, { useState } from "react";
+
 function Todo(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  function deleteHandler(){
-    setModalIsOpen(true);
-
+  function handleChange(e) {
+    setNewName(e.target.value);
   }
 
-  function closeModalHandler(){
-    setModalIsOpen(false)
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
   }
 
-  return (
-    <div className="card">
-      <h2>{props.text}</h2>
-      <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input id={props.id} className="todo-text" type="text" value={newName} onChange={handleChange} />
       </div>
-      {modalIsOpen? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />: null}
-      {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
-      
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
     </div>
   );
+
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
 
 export default Todo;
